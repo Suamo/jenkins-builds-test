f2f15d5793ffba372bbe44e4c9ab496cbc8a1d5a
Anton Vyshnevskyi <avyshnevskyi@vmware.com>
2022-05-22 15:20:50 +0300

    init

---
+++ src/main/java/com/example/jenkinsbuildstest/JenkinsBuildsTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jenkinsbuildstest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsBuildsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsBuildsTestApplication.class, args);
+    }
+
+}
